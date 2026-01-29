1e255b674d288fb48dbb300b9fda5e60fb7db7ea
Daniil <c.cffc@mail.ru>
2026-01-29 22:02:41 +1000

    fix tsconfig

--- server/api/send.post.ts
+++ server/api/send.post.ts
@@ -1,19 +1,21 @@
-import type { IProduct } from "~/types/api";
+import type { IProduct } from '~/types/api';
 
-export default defineEventHandler(async (event): Promise<{ success: boolean }> => {
-  const body = await readBody<{
-    id: number;
-    products: IProduct[];
-  }>(event);
+export default defineEventHandler(
+  async (event): Promise<{ success: boolean }> => {
+    const body = await readBody<{
+      id: number;
+      products: IProduct[];
+    }>(event);
 
-  if (!body?.id || !Array.isArray(body.products)) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: 'Invalid payload',
-    });
-  }
+    if (!body?.id || !Array.isArray(body.products)) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Invalid payload',
+      });
+    }
 
-  console.log('SAVE APPLICATION', body);
+    console.log('SAVE APPLICATION', body);
 
-  return { success: true };
-});
+    return { success: true };
+  },
+);
